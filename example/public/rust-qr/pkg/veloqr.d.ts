30f7223dc188f59f74c5a7c4df68f2746082d134
Pham Van Khang <phamvankhang.tvi@gmail.com>
2026-01-18 22:43:32 +0700

    feat: Upgrade WASM module, adding `crop_image`, `sharpen_image`, and `parse_mrz_text` functions, and updating `decode_qr_

--- example/public/rust-qr/pkg/veloqr.d.ts
+++ example/public/rust-qr/pkg/veloqr.d.ts
@@ -1,54 +1,60 @@
 /* tslint:disable */
 /* eslint-disable */
+
+export function crop_image(image_data: Uint8Array, width: number, height: number, x: number, y: number, crop_width: number, crop_height: number): Uint8Array;
+
 /**
  * Decode QR codes from image data (RGBA format)
  * Returns a JSON string containing an array of detected QR codes
  */
 export function decode_qr_from_image(image_data: Uint8Array, width: number, height: number): any;
+
 /**
  * Initialize the WASM module
  */
 export function init(): void;
+
 /**
  * Parse MRZ text lines to extract structured data
  */
 export function parse_mrz_text(mrz_text: string): any;
-export function crop_image(image_data: Uint8Array, width: number, height: number, x: number, y: number, crop_width: number, crop_height: number): Uint8Array;
+
 export function sharpen_image(image_data: Uint8Array, width: number, height: number, amount: number): Uint8Array;
 
 export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
 
 export interface InitOutput {
-  readonly memory: WebAssembly.Memory;
-  readonly decode_qr_from_image: (a: number, b: number, c: number, d: number, e: number) => void;
-  readonly parse_mrz_text: (a: number, b: number, c: number) => void;
-  readonly crop_image: (a: number, b: number, c: number, d: number, e: number, f: number, g: number, h: number, i: number) => void;
-  readonly sharpen_image: (a: number, b: number, c: number, d: number, e: number, f: number) => void;
-  readonly init: () => void;
-  readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
-  readonly __wbindgen_export: (a: number, b: number) => number;
-  readonly __wbindgen_export2: (a: number, b: number, c: number, d: number) => number;
-  readonly __wbindgen_export3: (a: number, b: number, c: number) => void;
-  readonly __wbindgen_start: () => void;
+    readonly memory: WebAssembly.Memory;
+    readonly decode_qr_from_image: (a: number, b: number, c: number, d: number, e: number) => void;
+    readonly parse_mrz_text: (a: number, b: number, c: number) => void;
+    readonly crop_image: (a: number, b: number, c: number, d: number, e: number, f: number, g: number, h: number, i: number) => void;
+    readonly sharpen_image: (a: number, b: number, c: number, d: number, e: number, f: number) => void;
+    readonly init: () => void;
+    readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
+    readonly __wbindgen_export: (a: number, b: number) => number;
+    readonly __wbindgen_export2: (a: number, b: number, c: number) => void;
+    readonly __wbindgen_export3: (a: number, b: number, c: number, d: number) => number;
+    readonly __wbindgen_start: () => void;
 }
 
 export type SyncInitInput = BufferSource | WebAssembly.Module;
+
 /**
-* Instantiates the given `module`, which can either be bytes or
-* a precompiled `WebAssembly.Module`.
-*
-* @param {{ module: SyncInitInput }} module - Passing `SyncInitInput` directly is deprecated.
-*
-* @returns {InitOutput}
-*/
+ * Instantiates the given `module`, which can either be bytes or
+ * a precompiled `WebAssembly.Module`.
+ *
+ * @param {{ module: SyncInitInput }} module - Passing `SyncInitInput` directly is deprecated.
+ *
+ * @returns {InitOutput}
+ */
 export function initSync(module: { module: SyncInitInput } | SyncInitInput): InitOutput;
 
 /**
-* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
-* for everything else, calls `WebAssembly.instantiate` directly.
-*
-* @param {{ module_or_path: InitInput | Promise<InitInput> }} module_or_path - Passing `InitInput` directly is deprecated.
-*
-* @returns {Promise<InitOutput>}
-*/
+ * If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+ * for everything else, calls `WebAssembly.instantiate` directly.
+ *
+ * @param {{ module_or_path: InitInput | Promise<InitInput> }} module_or_path - Passing `InitInput` directly is deprecated.
+ *
+ * @returns {Promise<InitOutput>}
+ */
 export default function __wbg_init (module_or_path?: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>): Promise<InitOutput>;
